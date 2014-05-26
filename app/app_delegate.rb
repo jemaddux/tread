59a642ba4d9b4a3669374084b4a91b56a2ccbe00
John Maddux <jemaddux@gmail.com>
2014-05-26 18:10:15 -0400

    screen

--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -1,5 +1,7 @@
-class AppDelegate
-  def application(application, didFinishLaunchingWithOptions:launchOptions)
-    true
+class AppDelegate < PM::Delegate
+  status_bar true, animation: :none
+
+  def on_load(app, options)
+    open HomeScreen.new(nav_bar: true)
   end
 end
